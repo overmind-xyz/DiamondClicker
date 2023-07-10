fd4477da20962706bdf9c44849f1f7f6393b0af8
John Yang <ys@mirny.io>
2023-07-10 18:17:37 +0900

    add nodeinfra endpoint, and remove unused url and client

--- web/lib/constants.ts
+++ web/lib/constants.ts
@@ -1,10 +1,6 @@
 import { AptosClient } from "aptos";
 
-export const NODE_URL = "https://fullnode.mainnet.aptoslabs.com";
-export const restClient = new AptosClient(NODE_URL, {
-  WITH_CREDENTIALS: false,
-});
-export const aptosClient = new AptosClient("https://fullnode.mainnet.aptoslabs.com/v1");
+export const aptosClient = new AptosClient("https://aptos-mainnet.nodeinfra.com/fullnode/v1");
 export const CONTRACT_ADDRESS = "0xe9fa01aaa1a483c6c598d470ae9b8174226590499b25812d8f0e86192a3b8711";
 
 export const UPGRADES = {
